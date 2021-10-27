01c0b3e6e00a27defbf4a9c47e77564032b8cd97
Jelani Woods <jelani@firstdraft.com>
2021-10-27 17:54:34 -0500

    No need numericality

--- app/models/interview.rb
+++ app/models/interview.rb
@@ -4,6 +4,6 @@ class Interview < ApplicationRecord
   has_one  :user, through: :application, source: :user
   has_one  :company, through: :application, source: :company
   validates :starts_at, presence: true
-  validates :round, presence: true, numericality: { less_than_or_equal_to: 3 }
+  # validates :round, presence: true, numericality: { less_than_or_equal_to: 3 }
   validates :round, uniqueness: { scope: ["application_id"] }
 end
